596d238090d94911c4f85f249e958781aeb2c452
tkmagesh <tkmagesh77@gmail.com>
2015-09-24 17:01:46 +0530

    Day-1 Complete

---
+++ Day-01/03-Closure/primeFinder.js
@@ -0,0 +1,42 @@
+function getPrimeFinder(){
+    var cache = {};
+    function isPrime(n){
+        console.log("processing ", n);
+        if (n <=3) return true;
+        for(var i=2; i <= (n/2); i++)
+            if (n % i === 0) return false;
+        return true;
+    }
+    return function(n){
+        if (typeof cache[n] === "undefined")
+            cache[n] = isPrime(n);
+        return cache[n];
+    }
+}
+
+
+function getAdder(){
+    var cache = {};
+    function add(x,y){
+        console.log("processing ", x , " and ", y);
+        return x + y;
+    }
+    return function(x, y){
+        var key = x + '-' + y;
+        if (typeof cache[key] === "undefined")
+            cache[key] = add(x,y);
+        return cache[key];
+    }
+}
+
+
+function memoize(fn){
+    var cache = {};
+    return function(){
+        var key = JSON.stringify(arguments);
+        if (typeof cache[key] === "undefined")
+            cache[key] = fn.apply(this,arguments);
+        return cache[key];
+    }
+}
+
