6b002167f65f2edc539ed780b5c376d8472c0187
tkmagesh <tkmagesh77@gmail.com>
2015-10-10 14:52:53 +0530

    Web Worker - done

--- Day-04/15-WebWorkers/doWork.js
+++ Day-04/15-WebWorkers/doWork.js
@@ -1,13 +1,22 @@
 function performTask(){
-    for(var i=0; i<10000; i++)
+    for(var i=0; i<10000; i++){
+
         for(var j=0; j<10000; j++)
             for(var k=0; k<100; k++){}
+        if (i % 100 === 0){
+            var percentCompleted = (i/10000) * 100;
+            self.postMessage({
+                type : 'progress',
+                percentCompleted : percentCompleted
+            });
+        }
+    }
 }
 
 self.addEventListener("message", function(evtArg){
     if (evtArg.data === 'start'){
         performTask();
-        self.postMessage("done");
+        self.postMessage({type : "done"});
     } else {
         console.log("Unknown message ", evtArg.data);
     }
